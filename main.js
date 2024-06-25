9079823e23aa64af5024122bdda004ccb9e4b7b2
johnyevsukov <jyevsukov@gmail.com>
2024-06-24 20:26:28 -0400

    flesh out index.html, main.js, and style.css file basics

---
+++ main.js
@@ -0,0 +1,9 @@
+/**
+ * @type { HTMLCanvasElement }
+ */
+
+// setup canvas
+const canvas = document.getElementById("mainCanvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 500;
