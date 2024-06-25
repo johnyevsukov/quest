0cdba598284056ac9ce522927b211c72e19f413d
johnyevsukov <jyevsukov@gmail.com>
2024-06-25 19:54:47 -0400

    add game class and animation loop

--- main.js
+++ main.js
@@ -2,8 +2,34 @@
  * @type { HTMLCanvasElement }
  */
 
-// setup canvas
+// set up canvas
 const canvas = document.getElementById("mainCanvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 500;
+
+// set up game
+class Game {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+  update(deltaTime) {}
+  draw(ctx) {}
+}
+
+const game = new Game(canvas.width, canvas.height);
+
+let lastTime = 0;
+
+// animation loop
+function animate(timeStamp) {
+  const deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  game.update(deltaTime);
+  game.draw(ctx);
+  requestAnimationFrame(animate);
+}
+
+animate(0);
