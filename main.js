423dac0bdfa2616b34074c867dd85e162a9b71c6
johnyevsukov <jyevsukov@gmail.com>
2024-07-09 14:12:50 -0400

    add basic player movement and sprite animations for walking and idle states

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 500;
 
+ctx.imageSmoothingEnabled = false;
+
 // set up game
 class Game {
   constructor(width, height) {
@@ -18,12 +20,13 @@ class Game {
     this.height = height;
     this.player = new Player(this);
     this.input = new InputHandler(this);
+    this.fps = 60;
   }
   update(deltaTime) {
     this.player.update(deltaTime);
   }
-  draw(ctx) {
-    this.player.draw(ctx);
+  draw(ctx, deltaTime) {
+    this.player.draw(ctx, deltaTime);
   }
 }
 
@@ -33,11 +36,13 @@ let lastTime = 0;
 
 // animation loop
 function animate(timeStamp) {
+  // ensure delta time is calculated in seconds
+  timeStamp *= 0.001;
   const deltaTime = timeStamp - lastTime;
   lastTime = timeStamp;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   game.update(deltaTime);
-  game.draw(ctx);
+  game.draw(ctx, deltaTime);
   requestAnimationFrame(animate);
 }
 
--- player.js
+++ player.js
@@ -5,32 +5,88 @@
 export class Player {
   constructor(game) {
     this.game = game;
-    this.width = 20;
-    this.height = 20;
+    this.spriteWidth = 100;
+    this.spriteHeight = 100;
+    this.width = this.spriteWidth * 1.5;
+    this.height = this.spriteHeight * 1.5;
     this.x = 20;
     this.y = 20;
-    this.speed = 5;
-    this.color = "blue";
+    this.speed = 100;
+    this.image = document.getElementById("playerSprite");
+    this.animationCount = 0;
+    this.animationInterval = 0.05;
+    this.currentSpriteFrame = 0;
+    this.states = {
+      idle: {
+        sprite: {
+          frames: 6,
+          frameY: 0,
+        },
+      },
+      walk: {
+        sprite: {
+          frames: 8,
+          frameY: 100,
+        },
+      },
+    };
+    this.currentState = this.states.idle;
   }
+
   update(deltaTime) {
     // player key input movement
     if (this.game.input.keys.includes("w")) {
-      this.y -= this.speed;
+      this.y -= this.speed * deltaTime;
     }
     if (this.game.input.keys.includes("s")) {
-      this.y += this.speed;
+      this.y += this.speed * deltaTime;
     }
     if (this.game.input.keys.includes("a")) {
-      this.x -= this.speed;
+      this.x -= this.speed * deltaTime;
     }
     if (this.game.input.keys.includes("d")) {
-      this.x += this.speed;
+      this.x += this.speed * deltaTime;
+    }
+    if (
+      this.game.input.keys.includes("w") ||
+      this.game.input.keys.includes("s") ||
+      this.game.input.keys.includes("a") ||
+      this.game.input.keys.includes("d")
+    ) {
+      this.currentState = this.states.walk;
+    } else {
+      this.currentState = this.states.idle;
     }
   }
-  draw(ctx) {
+
+  draw(ctx, deltaTime) {
     ctx.save();
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+
+    // delay sprite animation by animationInterval (0.05s)
+    if (this.animationCount < this.animationInterval) {
+      this.animationCount += deltaTime;
+    } else {
+      this.currentSpriteFrame++;
+      this.animationCount = 0;
+    }
+
+    let spriteFrameX =
+      (Math.floor(this.currentSpriteFrame) % this.currentState.sprite.frames) *
+      this.spriteWidth;
+    let spriteFrameY = this.currentState.sprite.frameY;
+
+    ctx.drawImage(
+      this.image,
+      spriteFrameX,
+      spriteFrameY,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+
     ctx.restore();
   }
 }
